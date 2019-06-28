8ac713107f1bd33abac65b66de8a605734afa41e
Vagner Nascimento <vagnernasc88@gmail.com>
2019-06-27 21:32:13 -0300

    Implemented PATCH operation

--- http-hapi/api/tests/apiHeroes.test.js
+++ http-hapi/api/tests/apiHeroes.test.js
@@ -2,9 +2,23 @@ const assert = require('assert')
 const api = require('./../api')
 let app = {}
 
+function insert() {
+    return app.inject({
+        method: 'POST',
+        url: '/heroes',
+        payload: {
+            nome: 'Flash',
+            poder: 'Speed'
+        }
+    });
+}
+
 describe.only('API Heroes test suite', function ()  {
+    
     this.beforeAll(async () => {
         app = await api
+        const result = await insert()
+        MOCK_ID = JSON.parse(result.payload)._id
     })
 
     it('GET /heroes', async () => {
@@ -45,5 +59,18 @@ describe.only('API Heroes test suite', function ()  {
         const payload = JSON.parse(result.payload)
         assert.deepEqual(result.statusCode, 400)
         assert.ok(payload.message.search('"nome" is required') !== -1)
+    })
+
+    it('PATCH /heroes/{id}', async () => {
+        const result = await app.inject({
+            method: 'PATCH',
+            url: `/heroes/${MOCK_ID}`,
+            payload: {
+                nome: 'Marvel Captain',
+                poder: 'All'
+            }
+        })
+        assert.deepEqual(result.statusCode, 200) 
+        assert.deepEqual(JSON.parse(result.payload).nModified, 1)
     })
 })
